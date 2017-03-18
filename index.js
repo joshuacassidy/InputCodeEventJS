549b245a87850b6ad1e16cf2df0d7bc92aeeafd4
joshuacassidy <Joshcassidy79@gmail.com>
2017-03-18 22:55:04 +0000

    Cleaning up code

--- index.js
+++ index.js
@@ -11,50 +11,9 @@ function success() {
 document.addEventListener('keydown', function(e) {
   var key = e.key;
 
-  // var userinput = code[CodePosition];
-
 key ==  code[CodePosition] ?  (function() {
-
-CodePosition++
-CodePosition === code.length ?  success() : null
-
+    CodePosition++
+    CodePosition === code.length ?  success() : null
 }())  : CodePosition = 0;
 
-
-
-  
-
-
-
-
-//   var x = 1;
-// (function(x) {
-//   console.log(x);
-// }(x))
-//
-// var y = false;
-// !function(y) {
-//   console.log(y);
-// }(true)
-//
-//
-// if(alert()){
-//  userinput = "";
-//   CodePosition = 0;
-// }
-
-
-//   if (key == userinput) {
-//
-//      CodePosition++;
-//
-//     if ( CodePosition == code.length)
-//       alert();
-//       // if(alert()){
-//       //    CodePosition = 0;
-//       //
-//       // }
-//   } else{
-//      CodePosition = 0;
-// }
 });
